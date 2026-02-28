b38048a070d1bae5c696afa8d91c5cc1e457a567
Illia Ladyha <iladyga98@gmail.com>
2026-02-28 18:44:43 +0100

    bug fixes

--- my-app/lib/supabase/middleware.ts
+++ my-app/lib/supabase/middleware.ts
@@ -6,11 +6,16 @@ export async function updateSession(request: NextRequest) {
     request,
   })
 
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+  const supabaseKey =
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ??
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY
+
   // With Fluid compute, don't put this client in a global environment
   // variable. Always create a new one on each request.
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    supabaseUrl,
+    supabaseKey!,
     {
       cookies: {
         getAll() {
